4c0c55f4c453e5dedf52e2ebfb6afec87b71b086
Shigehiro IDANI <idani@exp-t.com>
2019-11-27 15:15:42 +0900

    ブログの一覧、詳細、投稿の一部まで

--- src/src/Controller/BlogController.php
+++ src/src/Controller/BlogController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Post;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends AbstractController
 {
@@ -12,8 +15,55 @@ class BlogController extends AbstractController
      */
     public function index()
     {
+        $post = $this->getDoctrine()->getRepository(Post::class)->findAll();
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'posts' => $post,
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{id}", name="blog_show", requirements={"id"="\d+"})
+     *
+     * @param [type] $id
+     * @return void
+     */
+    public function show($id)
+    {
+        $post = $this->getDoctrine()->getRepository(Post::class)->find($id);
+        if (!$post) {
+            throw $this->createNotFoundException("The post does not exists");
+        }
+
+        return $this->render('blog/show.html.twig', ['post' => $post]);
+    }
+
+    /**
+     * @Route("/blog/new", name="blog_new")
+     *
+     * @return void
+     */
+    public function new(Request $request)
+    {
+        $post = new Post();
+        $form = $this->createFormBuilder($post)
+            ->add('title')
+            ->add('content')
+            ->add('save', SubmitType::class, ['label' => '作成する'])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setCreatedAt(new \DateTime());
+            $post->setUpdatedAt(new \DateTime());
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('blog');
+        }
+
+        return $this->render('blog/new.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
